68cfe4d93a381b61ade94318211ea2a509e4e331
Ben Puderer <bpuderer@yahoo.com>
2019-01-21 19:58:43 -0500

    diff workaround

--- services/doubler/__init__.py
+++ services/doubler/__init__.py
@@ -1,5 +1,11 @@
 import os
 import sys
 
-here = os.path.join(os.getcwd(), 'services', 'doubler')
-sys.path.append(here)
+
+# issue is that the current working directory is where run_tests.py is run
+# thus import doubler_pb2 as doubler__pb2
+# fails but import services.doubler.doubler_pb2 as doubler__pb2 works
+
+# https://stackoverflow.com/a/5137509
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(dir_path)

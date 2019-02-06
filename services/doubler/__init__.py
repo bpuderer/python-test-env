280c9ea820c937c023c8d854a4bf78cc1490d49f
Ben Puderer <bpuderer@yahoo.com>
2019-02-06 16:57:04 -0500

    dont mutate options

--- services/doubler/__init__.py
+++ services/doubler/__init__.py
@@ -3,8 +3,8 @@ import sys
 
 
 # issue is that the current working directory is where run_tests.py is run
-# thus import doubler_pb2 as doubler__pb2
-# fails but import services.doubler.doubler_pb2 as doubler__pb2 works
+# thus import doubler_pb2 as doubler__pb2 in doubler_pb2_grpc.py
+# does not work but import services.doubler.doubler_pb2 as doubler__pb2 works
 
 # https://stackoverflow.com/a/5137509
 dir_path = os.path.dirname(os.path.realpath(__file__))
--- utils/channel_factory.py
+++ utils/channel_factory.py
@@ -8,13 +8,15 @@ log = logging.getLogger(__name__)
 # https://grpc.io/grpc/python/grpc.html
 
 # https://grpc.io/grpc/core/group__grpc__arg__keys.html#details
-options = [('grpc.keepalive_time_ms', 15000),
-           ('grpc.keepalive_timeout_ms', 750),
-           ('grpc.keepalive_permit_without_calls', 1),
-           ('grpc.http2.max_pings_without_data', 0),
-          ]
+channel_options = [('grpc.keepalive_time_ms', 15000),
+                   ('grpc.keepalive_timeout_ms', 750),
+                   ('grpc.keepalive_permit_without_calls', 1),
+                   ('grpc.http2.max_pings_without_data', 0),
+                  ]
 
 def get_channel(host, port=50051, authority=None):
+    # dont mutate channel_options
+    options = channel_options.copy()
     if authority is not None:
         options.append(('grpc.default_authority', authority))
     log.debug(f'Creating insecure gRPC channel: {host}:{port} options={options}')
